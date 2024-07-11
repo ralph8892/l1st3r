738183d780a17ed0a8972bf897b172a322c9a228
ralph8892 <brufalralph@gmail.com>
2024-07-12 01:14:58 +0800

    Update item and reset UI state

--- script.js
+++ script.js
@@ -24,6 +24,16 @@ function onAddItemSubmit (e) {
     return;
   }
 
+  // Check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode');
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove('edit-mode');
+    itemToEdit.remove();
+    isEditMode = false;
+  }
+
   // Create item as DOM element
   addItemToDOM(newItem);
 
@@ -156,6 +166,7 @@ function filterItems (e) {
 }
 
 function checkUI () {
+  itemInput.value = '';
   const items = itemList.querySelectorAll('li');
   if (items.length === 0) {
     clearBtn.style.display = 'none';
@@ -164,6 +175,10 @@ function checkUI () {
     clearBtn.style.display = 'block';
     itemFilter.style.display = 'block';
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = '#333';
+  isEditMode = false;
 }
 
 // Initialize app
